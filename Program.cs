cf58f8b7b26ef7ebdeaba618d9e14863719c4937
jonicastro3 <Jonathan.emanuel.castro@mi.unc.edu.ar>
2026-05-04 06:55:08 -0300

    feat: definir clase tarea y lista base

--- Program.cs
+++ Program.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 
 namespace TaskFlow
-static void Main(string[] args)
-        {
-            listaTareas.Add(new Tarea { Nombre = "Aprender Git", Estado = "Completada" });
-            listaTareas.Add(new Tarea { Nombre = "Terminar Actividad 4" }); // Nace Pendiente
-            
-            Console.WriteLine("Datos cargados en el sistema...");
-        }
 {
+    // 1. Creamos la estructura de la tarea
     class Tarea
     {
         public string Nombre { get; set; }
@@ -18,7 +12,25 @@ static void Main(string[] args)
 
     class Program
     {
+        // 2. Creamos la lista de tareas
         static List<Tarea> listaTareas = new List<Tarea>();
-        static void Main(string[] args) { }
+
+        // 3. El único método Main que ejecuta el programa
+        static void Main(string[] args)
+        {
+            // --- AVANCE 2: Carga de datos ---
+            listaTareas.Add(new Tarea { Nombre = "Aprender Git", Estado = "Completada" });
+            listaTareas.Add(new Tarea { Nombre = "Terminar Actividad 4" }); // Nace Pendiente
+            
+            Console.WriteLine("Datos cargados en el sistema...");
+
+            // --- AVANCE 3: Visualización funcional ---
+            Console.WriteLine("\n--- TaskFlow: Monitoreo de Estados ---");
+            foreach (var t in listaTareas)
+            {
+                string icono = t.Estado == "Completada" ? "[X]" : "[ ]";
+                Console.WriteLine($"{icono} {t.Nombre} | Estado: {t.Estado}");
+            }
+        }
     }
 }
