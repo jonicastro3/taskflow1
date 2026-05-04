7515fd435f28407a1b303f10b41ab69f3d990391
jonicastro3 <Jonathan.emanuel.castro@mi.unc.edu.ar>
2026-05-04 06:52:30 -0300

    feat: agregar carga inicial de tareas con estados

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 
 namespace TaskFlow
+static void Main(string[] args)
+        {
+            listaTareas.Add(new Tarea { Nombre = "Aprender Git", Estado = "Completada" });
+            listaTareas.Add(new Tarea { Nombre = "Terminar Actividad 4" }); // Nace Pendiente
+            
+            Console.WriteLine("Datos cargados en el sistema...");
+        }
 {
     class Tarea
     {
