845dbda663ced2ec64fb1922a506258986e45f32
jonicastro3 <Jonathan.emanuel.castro@mi.unc.edu.ar>
2026-05-04 06:06:31 -0300

    feat: agregar validación de nombre vacio

--- Program.cs
+++ Program.cs
@@ -8,6 +8,14 @@ namespace TaskFlow
         {
             Console.WriteLine("--- Sistema TaskFlow: Crear Tarea ---");
             Console.Write("Ingrese el nombre de la tarea: ");
+            if (string.IsNullOrEmpty(nombreTarea))
+            {
+                Console.WriteLine("Error: El nombre no puede estar vacío.");
+            }
+            else 
+            {
+                Console.WriteLine($"Tarea '{nombreTarea}' capturada correctamente.");
+            }
             string nombreTarea = Console.ReadLine();
             
             // TODO: Agregar validaciones y guardado
